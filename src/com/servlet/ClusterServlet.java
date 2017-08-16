0468771b95a15b349134e8cf77b070dc454bb249
Mert Erpam <merterpam@users.noreply.github.com>
2017-08-16 13:59:05 +0300

    Encapsulation of field
    Encapsulation of static field

--- src/com/servlet/ClusterServlet.java
+++ src/com/servlet/ClusterServlet.java
@@ -3,7 +3,10 @@ package com.servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -14,8 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.application.ClusterApplication;
+import com.erpam.mert.ST_TWEC.model.Tweet;
 import com.erpam.mert.utils.io.WordEmbeddingLoader;
 import com.models.response.ClusterResponse;
+import com.models.response.ErrorResponse;
 import com.utils.Utility;
 
 /**
@@ -24,14 +29,21 @@ import com.utils.Utility;
 @WebServlet(loadOnStartup=10,urlPatterns={"/uploadfile"})
 @MultipartConfig
 public class ClusterServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	public static WordEmbeddingLoader wordEmbeddingDict = null;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public ClusterServlet() {
-		super();
+	private static WordEmbeddingLoader wordEmbeddingDict;
+
+	private static final String wordEmbeddingsPath = "/Users/mert/Documents/WordEmbedding/GoogleNews-vectors-negative300.bin";
+
+	private static final long serialVersionUID = 1L;
+
+
+
+	public static WordEmbeddingLoader getWordEmbeddingDict() {
+		return wordEmbeddingDict;
+	}
+
+	public static void setWordEmbeddingDict(WordEmbeddingLoader wordEmbeddingDict) {
+		ClusterServlet.wordEmbeddingDict = wordEmbeddingDict;
 	}
 
 	/**
@@ -41,11 +53,18 @@ public class ClusterServlet extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		if(wordEmbeddingDict == null)
+		if(getWordEmbeddingDict() == null)
 		{
-			String wDirectory = "/Users/mert/Documents/WordEmbedding/GoogleNews-vectors-negative300.bin";
-			wordEmbeddingDict = new WordEmbeddingLoader(wDirectory);
-			wordEmbeddingDict.generateModel();
+			String wDirectory = "";
+			try {
+				wDirectory = (String) new InitialContext().lookup("java:comp/env/com.I-TWEC.wordEmbeddingsPath");
+			} catch (NamingException e) {
+				wDirectory = wordEmbeddingsPath;
+				e.printStackTrace();
+			}
+
+			setWordEmbeddingDict(new WordEmbeddingLoader(wDirectory));
+			getWordEmbeddingDict().generateModel();
 		}
 	}
 
@@ -64,7 +83,7 @@ public class ClusterServlet extends HttpServlet {
 		application.setClusterThreshold(Float.parseFloat(request.getParameter("clusterThreshold")));
 
 		application.setEmbeddingDimension(Integer.parseInt(request.getParameter("embeddingDimension")));
-		application.setWordEmbeddingDict(wordEmbeddingDict);
+		application.setWordEmbeddingDict(getWordEmbeddingDict());
 		application.setClusterLimit(Integer.parseInt(request.getParameter("clusterLimit")));
 		application.setSentimentThreshold(Float.parseFloat(request.getParameter("sentimentThreshold")));
 		application.setShortTextLength(Integer.parseInt(request.getParameter("shortTextLength")));
@@ -72,12 +91,30 @@ public class ClusterServlet extends HttpServlet {
 		BufferedReader inputReader = new BufferedReader(new InputStreamReader(request.getPart("file").getInputStream(), "UTF8"));
 		String fileName = request.getPart("file").getSubmittedFileName();
 		application.setFileName(fileName.substring(0,fileName.length()-4));
-		application.setTweets(Utility.readStream(inputReader));
 
-		ClusterResponse obj = application.clusterTweets();
+		ArrayList<Tweet> tweets = Utility.readStream(inputReader);
+
+		String responseMessage;
+		if(tweets != null) {
+			application.setTweets(tweets);
+			ClusterResponse obj = application.clusterTweets();
+			if(obj != null) {
+				if(obj.getClusterSize() != 0) {
+					responseMessage = obj.toJSON();
+				} else {
+					responseMessage = new ErrorResponse("With the given dataset, we could not form clusters").toJSON();
+				}
+			} else {
+				responseMessage = new ErrorResponse("We could not process the data. Please try again later or try with a smaller set of data.").toJSON();;
+			}
+		}
+		else {
+			responseMessage = new ErrorResponse("Invalid Data Format. \nPlease format your data and try again").toJSON(); 
+		}
 
 		response.setCharacterEncoding("UTF-8");
-		response.getWriter().println(obj.toJSON());
+		response.getWriter().println(responseMessage);
+
 	}
 
 }
