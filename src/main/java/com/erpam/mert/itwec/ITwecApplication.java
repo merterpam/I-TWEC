d469a80c6580d3847061b50315632669c8ca46f8
Mert Erpam <merterpam@users.noreply.github.com>
2017-09-12 15:39:22 +0300

    Spring Framework initialization

---
+++ src/main/java/com/erpam/mert/itwec/ITwecApplication.java
@@ -0,0 +1,12 @@
+package com.erpam.mert.itwec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITwecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITwecApplication.class, args);
+	}
+}
---
+++ src/test/java/com/erpam/mert/itwec/ITwecApplicationTests.java
@@ -0,0 +1,16 @@
+package com.erpam.mert.itwec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ITwecApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
